5603486907ae5c5fb61c63b45aad9e812cc8379b
A18850 <A18850@RI150WS3206.AMICA.com>
2018-05-01 15:40:28 -0400

    a

--- fsdafssdfsa/src/main/java/com/amica/escm/fsdafssdfsa/App3.java
+++ fsdafssdfsa/src/main/java/com/amica/escm/fsdafssdfsa/App3.java
@@ -13,5 +13,6 @@ public class App3
     	System.out.println( "Hello World!" );
     	System.out.println( "Hello World!" );
     	System.out.println( "Hello World!" );
+    	System.out.println( "Hello World!" );
     }
 }
